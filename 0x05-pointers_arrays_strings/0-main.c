72b3a1d7d8053946469680eb95cade55f286b82f
Bankole <bankoledami00@gmail.com>
2022-09-19 15:56:40 -0700

    My commits

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -6,14 +6,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int n;
 
 	n = 402;
-	printf("n = %d\n", n);
+	printf("n=%d\n", n);
 	reset_to_98(&n);
-	printf("n = %d\n", n);
+	printf("n=%d\n", n);
+
 	return (0);
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * reset_to_98 - Function takes pointer to an int as parameter.
- *
- * Return: 0
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: declaration of n and paramters for the function reset_to_98
+ * Return: Always 0.
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;

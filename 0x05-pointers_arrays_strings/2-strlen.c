770d37b3adba31eefef4a1d65c89583936702221
Bankole <bankoledami00@gmail.com>
2022-09-19 15:22:20 -0700

    My commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code for main.
+ * @s: declaration of *s and parameters for the function _strlen
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+
+		return (0);
+}

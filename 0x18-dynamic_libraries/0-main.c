a00aa125be074fe3251f19e94ab48cf30ad7d553
Bankole-Damilola <bankoledami00@gmail.com>
2022-12-12 17:52:18 -0800

    My commits

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0 if successful else 1
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+This directory butress our understanding on how linker works. This directory required me to create a dynamic library.
